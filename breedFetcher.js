6cd1b44b7773258150d362a90603231408d9d1c4
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-13 17:59:11 +0000

    refactored breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,19 @@
 const request = require('request');
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
-if (process.argv[2] === undefined) {
-  console.log("Breed not defined");
-} else {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-    }
-  });
+
+const breedFetcher = function() {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+  if (process.argv[2] === undefined) {
+    console.log("Breed not defined");
+  } else {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const data = JSON.parse(body);
+        console.log(data[0].description);
+      }
+    });
+  }
 }
+
+breedFetcher()
