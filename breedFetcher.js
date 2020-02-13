fd4c28fbd2a16248281ff767785c8de642621b27
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-13 17:53:37 +0000

    complete breedfetcher program

---
+++ breedFetcher.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+if (process.argv[2] === undefined) {
+  console.log("Breed not defined");
+} else {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    }
+  });
+}
